f891ae83054a9e03bff2f3e3f94ef4a0d9b3f938
danodoms <danodoms@gmail.com>
2024-11-11 12:01:29 +0800

    added check if tf is ready to use @ index

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -6,16 +6,27 @@ import * as Crypto from 'expo-crypto'
 import { useSystem } from 'src/powersync/System';
 import { RESULTS_TABLE, Result } from 'src/powersync/Schema';
 import { useState, useEffect } from 'react';
+import * as tf from '@tensorflow/tfjs';
+import '@tensorflow/tfjs-react-native';
+
 
 
 export default function TabOneScreen() {
   const [result, setResult] = useState('');
   const { supabaseConnector, db } = useSystem();
   const [results, setResults] = useState<Result[]>([]);
+  const [isTfReady, setIsTfReady] = useState(false); // Track TensorFlow readiness
 
   useEffect(() => {
     loadResults();
+    initializeTensorFlow();
   }, []);
+
+  const initializeTensorFlow = async () => {
+    // Wait for TensorFlow to be ready
+    await tf.ready();
+    setIsTfReady(true);
+  };
 
   const loadResults = async () => {
     const dbResults = await db.selectFrom(RESULTS_TABLE).selectAll().execute();
@@ -60,6 +71,13 @@ export default function TabOneScreen() {
 
   return (
     <YStack f={1} ai="center" gap="$4" p="$4" borderColor="white" borderWidth={0}>
+
+      {/* Loading TensorFlow message */}
+      {!isTfReady ? (
+        <Text>Loading TensorFlow...</Text>
+      ) : (
+        <Text>TensorFlow is ready to be used!</Text>
+      )}
 
       <Stack borderColor="white" borderWidth={0} borderRadius="$4" width="100%" flex={1} backgroundColor='$accentBackground' padding="$4" justifyContent="flex-end">
         <H2 textAlign='left'>XtraRice</H2>
